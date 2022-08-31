39616045fb902d1e0dca63a057341435cdb3dce9
ccbrantley <chrisbrantley1993@gmail.com>
2022-08-31 11:43:23 -0400

    Add files via upload

---
+++ DisplayingNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DisplayingNumbers
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("1 2 3 4");
+			Console.Write("1 ");
+			Console.Write("2 ");
+			Console.Write("3 ");
+			Console.Write("4\n");
+			int one = 1;
+			int two = 2;
+			int three = 3;
+			int four = 4;
+			Console.WriteLine($"{one} {two} {three} {four}");
+		}
+	}
+}
