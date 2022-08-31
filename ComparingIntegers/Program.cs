303c307f42aac9fc89dd330f00791ae5e9fa89a8
ccbrantley <chrisbrantley1993@gmail.com>
2022-08-31 19:11:54 -0400

    Add files via upload

---
+++ ComparingIntegers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ComparingIntegers
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("Please enter an integer: ");
+			string userFirstIntegerInput = Console.ReadLine();
+			Console.Write("Please enter another integer: ");
+			string userSecondIntegerInput = Console.ReadLine();
+			int firstNumber = Convert.ToInt32(userFirstIntegerInput);
+			int secondNumber = Convert.ToInt32(userSecondIntegerInput);
+			if (firstNumber > secondNumber)
+			{
+				Console.WriteLine($"{firstNumber} is larger.");
+			}
+			else if (firstNumber < secondNumber)
+			{
+				Console.WriteLine($"{secondNumber} is larger.");
+			}
+			else
+			{
+				Console.WriteLine("These numbers are equal.");
+			}
+		}
+	}
+}
