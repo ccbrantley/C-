6c710adc8900d273c5d9e3719f8006cff1d22b71
ccbrantley <chrisbrantley1993@gmail.com>
2022-08-31 19:23:30 -0400

    Add files via upload

---
+++ OddOrEven/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace OddOrEven
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("Please enter an integer: ");
+			string userInput = Console.ReadLine();
+			int number = Convert.ToInt32(userInput);
+			int remainder = number % 2;
+			if (remainder == 0)
+			{
+				Console.WriteLine($"{number} is even.");
+			}
+			else
+			{
+				Console.WriteLine($"{number} is odd.");
+			}
+		}
+	}
+}
