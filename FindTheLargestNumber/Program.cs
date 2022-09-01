d28a58491257cad4239c0af70968812d0e93e1f1
ccbrantley <chrisbrantley1993@gmail.com>
2022-08-31 20:33:06 -0400

    Add files via upload

---
+++ FindTheLargestNumber/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace FindTheLargestNumber
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			
+			int number;
+			int largest = int.MinValue;
+			Console.WriteLine("Please enter 10 numbers.");
+			for (int counter = 0; counter < 10; counter++)
+			{
+				if (counter == 0)
+				{
+					Console.Write("Enter a number ({0}): ", counter + 1);
+				}
+				else
+				{
+					Console.Write("Enter another number ({0}): ", counter + 1);
+				}
+				number = Convert.ToInt32(Console.ReadLine());
+				if (number > largest)
+				{
+					largest = number;
+				}
+			}
+			Console.WriteLine($"The largest number is: {largest}");
+		}
+	}
+}
