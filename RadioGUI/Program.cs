71342020c156674a9b40eea87a1c40162af747c3
ccbrantley <chrisbrantley1993@gmail.com>
2022-09-12 13:35:34 -0400

    Update Program.cs

--- RadioGUI/Program.cs
+++ RadioGUI/Program.cs
@@ -1,3 +1,9 @@
+/*
+2.7 (Notepad GUI) Create the GUI for the notepad as shown in Fig. 2.36
+	a) Manipulating the Form's properties.
+	b) Adding a MenuStrip control to the Form.
+	c) Adding a RichTextBox to the form.
+*/
 using System;
 using System.Collections.Generic;
 using System.Linq;
