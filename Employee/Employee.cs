fab61664c69fc3034f6d13fa408029e402ec48e2
ccbrantley <chrisbrantley1993@gmail.com>
2022-10-03 13:49:49 -0400

    Add files via upload

---
+++ Employee/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class Employee
+{
+	public string FirstName { get; set; }
+	public string LastName { get; set; }
+	private decimal monthlySalary = 0;
+
+	public Employee (string _firstName, string _lastName, decimal _monthlySalary)
+	{
+		FirstName = _firstName;
+		LastName = _lastName;
+		MonthlySalary = _monthlySalary;
+	}
+
+	public decimal MonthlySalary 
+	{
+		get
+		{
+			return monthlySalary;
+		}
+		set
+		{
+			if (value < 0)
+			{
+				return;
+			}
+			monthlySalary = value;
+		}
+	}
+
+}
