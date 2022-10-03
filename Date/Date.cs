bb1119a8b9470ba848abb85a1e09e7987c3cc70e
ccbrantley <chrisbrantley1993@gmail.com>
2022-10-03 14:01:52 -0400

    Add files via upload

---
+++ Date/Date.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class Date
+{
+	public int Month { get; set; }
+	public int Day { get; set; }
+	public int Year { get; set; }
+
+	public Date (int _month, int _day, int _year)
+	{
+		Month = _month;
+		Day = _day;
+		Year = _year;
+	}
+
+	public void DisplayDate ()
+	{
+		Console.WriteLine($"{Month}/{Day}/{Year}");
+	}
+
+}
---
+++ Date/DateTest.cs
@@ -0,0 +1,23 @@
+﻿// Application to test class Date.
+using System;
+
+class DateTest
+{
+   static void Main()
+   {
+      Date date1 = new Date(7, 4, 2004);
+
+      Console.Write("The initial date is: ");
+      date1.DisplayDate();
+
+      // change date values
+      date1.Month = 11;
+      date1.Day = 1;
+      date1.Year = 2003;
+
+      Console.Write("\nDate with new values is: ");
+      date1.DisplayDate();
+
+      Console.WriteLine(); // output a new line
+   } 
+}
