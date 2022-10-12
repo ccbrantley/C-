318ff36f0cbc5493f6ffb6e9f3752e2ccbbb0cd5
ccbrantley <chrisbrantley1993@gmail.com>
2022-10-12 11:26:40 -0400

    Add files via upload

---
+++ Project1/MysteryNumber.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project1
+{
+	class MysteryNumber
+	{
+		public int RandomNumber { get; }
+
+		public MysteryNumber()
+		{
+			RandomNumber = new Random().Next(1, 100);
+		}
+	}
+}
---
+++ Project1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Project1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			MysteryNumber mysteryNumber = new MysteryNumber();
+			int userGuesses = 5;
+			bool gameOverFlag = false;
+			while (userGuesses > 0 && !gameOverFlag)
+			{
+				userGuesses--;
+				Console.Write("Please enter a number: ");
+				int userGuess = int.Parse(Console.ReadLine());
+				if (mysteryNumber.RandomNumber == userGuess)
+				{
+					gameOverFlag = true;
+					Console.WriteLine("You have guessed correctly.");
+					Console.WriteLine($"It took you {5 - userGuesses} tries.");
+				}
+				else
+				{
+					Console.WriteLine("You have guessed incorrectly.");
+					Console.WriteLine($"You have made {5 - userGuesses} guesses.");
+					Console.WriteLine($"You have {userGuesses} tries left.");
+				}
+			}
+		}
+	}
+}
