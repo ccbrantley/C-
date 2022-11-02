a4b1fdb3d71634fe779f2ccd054835fc8a71407f
ccbrantley <chrisbrantley1993@gmail.com>
2022-11-02 15:33:54 -0400

    Add files via upload

---
+++ DisplayingTriangles/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text;
+
+namespace DisplayingTriangles
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			for (int i = 10; i > 0; i --)
+			{
+				for (int j = i; j < 11; j ++)
+				{
+					Console.Write("*");
+				}
+				Console.WriteLine();
+			}
+			Console.WriteLine();
+
+			for (int i = 10; i > 0; i--)
+			{
+				for (int j = i; j > 0; j --)
+				{
+					Console.Write("*");
+				}
+				Console.WriteLine();
+			}
+			Console.WriteLine();
+
+			for (int i = 10; i > 0; i--)
+			{
+				for (int j = i; j < 10; j ++)
+				{
+					Console.Write(" ");
+				}
+				for (int j = i; j > 0; j --)
+				{
+					Console.Write("*");
+				}
+				Console.WriteLine();
+			}
+			Console.WriteLine();
+
+			for (int i = 10; i > 0; i--)
+			{
+				for (int j = i; j > 0; j --)
+				{
+					Console.Write(" ");
+				}
+				for (int j = i; j < 10; j++)
+				{
+					Console.Write("*");
+				}
+				Console.WriteLine();
+			}
+			Console.ReadLine();
+
+		}
+	}
+}
