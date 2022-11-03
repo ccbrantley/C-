a5e1d8e27f64c6eac12857823bc9a99eb605f925
ccbrantley <chrisbrantley1993@gmail.com>
2022-11-03 09:49:21 -0400

    Add files via upload

---
+++ DisplayingABarChart/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DisplayingABarChart
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int counter = 0;
+			int userInput;
+			while (counter < 3)
+			{
+				Console.WriteLine("Please enter in a number (1 - 30): ");
+				userInput = int.Parse(Console.ReadLine());
+				if (userInput > 0 && userInput < 31)
+				{
+					for (int starCount = userInput; starCount > 0; starCount --)
+					{
+						Console.Write("*");
+					}
+					Console.WriteLine();
+					counter += 1;
+				}
+				else
+				{
+					Console.WriteLine("The input is invalid, please enter in a number within the allowed range.");
+				}
+			}
+		}
+	}
+}
