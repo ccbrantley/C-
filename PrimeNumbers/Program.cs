aab30fb952cd06cb8211befd877c76fc93e6f95f
ccbrantley <chrisbrantley1993@gmail.com>
2022-11-07 13:53:22 -0500

    Add files via upload

---
+++ PrimeNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PrimeNumbers
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			for (int i = 2; i < 10001; i++)
+			{
+				if (Prime(i))
+				{
+					Console.WriteLine($"{i} is a prime number");
+				}
+			}
+		}
+		static bool Prime (int _number)
+		{
+			int upperLimit = (int) (Math.Pow(_number, 1 / 2.0) + .5);
+			for (int i = 2; i < upperLimit + 1; i ++)
+			{
+				if ((_number % i) == 0)
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+	}
+}
