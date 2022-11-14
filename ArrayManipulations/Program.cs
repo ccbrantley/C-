404d9a962d380247e82de3faa26b577bf16e83d7
ccbrantley <chrisbrantley1993@gmail.com>
2022-11-14 10:53:54 -0500

    Add files via upload

---
+++ ArrayManipulations/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ArrayManipulations
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] array = {0, 0, 0};
+			for (int count = 0; count < array.Length; ++count)
+			{
+				array[count] += 1;
+			}
+			foreach (int elt in array)
+			{
+				Console.WriteLine(elt);
+			}
+		}
+	}
+}
