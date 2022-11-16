8bec0c3cb41caa16cbcaf3d190c3344b5a211fca
ccbrantley <chrisbrantley1993@gmail.com>
2022-11-16 10:48:48 -0500

    Add files via upload

---
+++ DuplicateElimination/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace DuplicateElimination
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] array = new int[5];
+			int count = 0;
+			while (count < array.Length)
+			{
+				Console.WriteLine("Enter an unique integer (10 to 100): ");
+				int userInput = int.Parse(Console.ReadLine());
+				if (userInput > 9 && userInput < 101)
+				{
+					bool unique = true;
+					for (int index = 0; index < count; ++index)
+					{
+						if (array[index] == userInput)
+						{
+							unique = false;
+							Console.WriteLine("The integer was not unique, please try again.");
+						}
+					}
+					if (unique)
+					{
+						array[count] = userInput;
+						++count;
+						Console.WriteLine($"The integer {userInput} was unique!");
+					}
+				}
+				else
+				{
+					Console.WriteLine("The input was invalid, please try again.");
+				}
+				Console.WriteLine();
+			}
+			Console.WriteLine("All unique integer inputs are listed below.");
+			foreach (int elt in array)
+			{
+				Console.WriteLine(elt);
+			}
+		}
+	}
+}
